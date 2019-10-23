e5680bb39d6c67a6c201d64ae8162d5c33c45339
Axel Axelsson <Axel92_x@hotmail.com>
2019-10-23 16:20:32 +0200

    Initial Commit

---
+++ HelloSingleView2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloSingleView2
+//
+//  Created by Axel Axelsson on 2019-10-23.
+//  Copyright © 2019 Axel Axelsson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
